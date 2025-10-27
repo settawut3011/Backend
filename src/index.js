3cc6f27d826f8e29cb01c4d89fff542aace373ba
Settawut <ok_301138@outlook.co.th>
2025-10-27 21:28:54 +0700

    เรียน คำสั่ง delete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// ** งานหลังบ้าน Backend **
+// ประกาศตัวแปลต่างๆ
 const express = require("express");
 const app = express();
 const port = 3000;
 const inventory = require("./inventory.js");
 
+//  ชุดคำสั่งโค้ดต่างๆ
+
 app.use(express.urlencoded());
+
+// ชุดคำสั่ง ลบข้อมูล (delete)
+app.delete("/listProduct/:id", (req, res) => {
+  const deleteId = req.params.id;
+  const deleteProduct = inventory.deleteProduct(deleteId);
+  res.send(deleteProduct);
+});
+
+// ชุดคำสั่ง Update (Put)
 app.put("/listProduct/:id", (req, res) => {
   const updateId = req.params.id;
   const updateName = req.body.name;
@@ -11,6 +24,7 @@ app.put("/listProduct/:id", (req, res) => {
   res.send(updatedProduct);
 });
 
+// ชุดคำสั่ง เพิ่มข้อมูล (Post)
 app.post("/listProduct", (req, res) => {
   const newproduct = inventory.nameproduct(req.body.name);
 
--- src/inventory.js
+++ src/inventory.js
@@ -27,20 +27,41 @@ function nameproduct(name) {
 
 // สำหรับค้นหาแค่ตัวแปลในอาเรย์แบบเจาะจง
 // function updatedata(id,name) {
-//    const y = listProduct[].find(({ id }) => {
-//       return id === Number(x);
-//     });
+//    const y = listProduct[] ลำดับ เริ่มนับจาก 0
 
 // อัปเดตข้อมูลสินค้า โดยหาสินค้าที่มีidที่userกำหนดและอัปเดตnameข้อมูลของสินค้านั้น
 function updatedata(id, name) {
+  // findindex คือ หาลำดับ  ค่าที่ได้จากตัวแปลxจะเท่ากับ = ค่าลำดับ แต่ถ้าไม่เจอจะส่งค่า -1 กลับมา
+  const x = listProduct.findIndex((product) => {
+    // Number(id) คือการแปลงค่าที่ user ใส่เข้ามาและแปลงเป็นตัวเลข จากนั้นเปรียบเทียบค่าที่กรอกมานั้น เท่ากับเลขid
+    return Number(id) === product.id;
+  });
+
+  if (x !== -1) {
+    const newProduct = { id, name };
+    // splice ทำหน้าที่ลบค่าในอาเรย์ และ อัปเดทค่าในอาเรย์
+    listProduct.splice(x, 1, newProduct); //x = ลำดับข้อมูลที่จะอัปเดต , 1 = จำนวนข้อมูลที่จะอัปเดต , newProduct = { id, name } แทนที่ลำดับข้อมูลที่กำหนดไว้ นั้นคือxของเรา
+    return newProduct;
+  } else {
+    return null;
+  }
+}
+
+// ฟังก์ชันสำหรับลบข้อมูล
+// 1.หาตำแหน่งของสินค้า โดยที่ลูกค้าบอกผ่านไอดี id
+// 2.กำหนดเงื่อนไข ถ้าหาสินค้าไม่เจอ ให้บอกลูกค้ากลับไปว่าไม่รู้  แต่  ถ้ารู้ให้ดำเนินการขั้นตอนถัดไป
+// 3.ให้ดำเนินการนำสินค้าที่พบตำแหน่งนั้นไปทิ้ง นั้นคือลบ {} แปลว่าลบสินค้านั้นออกไป
+
+function deleteProduct(id) {
   const x = listProduct.findIndex((product) => {
     return Number(id) === product.id;
   });
 
   if (x !== -1) {
-    const y = { id, name };
-    listProduct.splice(x, 1, y); //x = ลำดับข้อมูลที่จะอัปเดต , 1 = จำนวนข้อมูลที่จะอัปเดต , y = { id, name } แทนที่ลำดับข้อมูลที่กำหนดไว้ นั้นคือxของเรา
-    return y;
+    // splice ทำหน้าที่ลบค่าในอาเรย์ และ อัปเดทค่าในอาเรย์
+    //x = ลำดับข้อมูลที่จะอัปเดต , 1 = จำนวนข้อมูลที่จะอัปเดต , newProduct = { id, name } แทนที่ลำดับข้อมูลที่กำหนดไว้ นั้นคือxของเรา
+    const deletedProduct = listProduct.splice(x, 1);
+    return deletedProduct;
   } else {
     return null;
   }
@@ -50,4 +71,5 @@ module.exports = {
   listProduct,
   nameproduct,
   updatedata,
+  deleteProduct,
 };

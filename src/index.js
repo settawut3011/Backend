9cbc78d4c47430305c9da3a7bfe4db2c58778d49
Settawut <ok_301138@outlook.co.th>
2025-10-29 22:37:59 +0700

    ทำโจทย์ Backend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 // ** งานหลังบ้าน Backend **
-// ประกาศตัวแปลต่างๆ
+
+// คำสั่งขั้นตอนในการติดตั้ง
+// 1. npm init -y
+// 2. npm install express
+// 3. npm install nodemon
+// 4. npm run start  เพิ่ม script ใน package.json โดยใส่ Key:value  "start": "node ./src/index.js",
+// 5. npm run dev    เพิ่ม script ใน package.json โดยใส่ Key:value  "dev": "npx nodemon ./src/index.js"
+
+// คำสั่งเรียกเครื่องมือ express
 const express = require("express");
+// คำสั่งใช้เครื่องมือ express
 const app = express();
+// กำหนด port ที่จะใช้แสดงผลหน้าเว็ป
 const port = 3000;
+// คำสั่งนำเข้าข้อมูลทั้งหมดจากไฟล์ inventory.js
 const inventory = require("./inventory.js");
+// คำสั่งนำเข้าข้อมูลทั้งหมดจากไฟล์ ???
+const nodemon = require("nodemon");
 
-//  ชุดคำสั่งโค้ดต่างๆ
-
+//  ชุดคำสั่งแปลงข้อมูลที่ถูกส่งมาจากฟอร์ม (HTML form) ผ่าน HTTP POST กับ PUT ให้อยู่ในรูปแบบที่สามารถเข้าถึงได้จาก req.body
 app.use(express.urlencoded());
+
+// ชุดคำสั่งด้วย 4 Methods (API CRUD) ต่างๆ ได้แก่ เพิ่ม อ่าน แก้ไข ลบ (Post,Get,Update,Delete)
+// v
+// v
+// v
 
 // ชุดคำสั่ง ลบข้อมูล (delete)
 app.delete("/listProduct/:id", (req, res) => {
@@ -33,24 +50,28 @@ app.post("/listProduct", (req, res) => {
   console.log(req.body);
 });
 
+// ชุดคำสั่ง อ่านข้อมูล (Get)
+// แสดงผลข้อมูล "Hello N'Nut" ผ่านหน้าเว็ป
 app.get("/", (req, res) => {
   res.send("Hello N'Nut");
 });
 
+// แสดงผลข้อมูลใน listProduct ทั้งหมดผ่านหน้าเว็ปช่องทาง /listProduct
 app.get("/listProduct", (req, res) => {
   res.send(inventory.listProduct);
 });
 
+// ชุดคำสั่ง อ่านข้อมูล (Get) โดยค้นหาค่าเฉพาะ parameter id ที่ user ร้องขอ
 app.get("/listProduct/:id", (req, res) => {
-  const x = req.params.id;
-  console.log(x);
-  const id = x;
+  const userReqid = req.params.id;
+  console.log(userReqid);
   const y = inventory.listProduct.find(({ id }) => {
-    return id == x;
+    return id == userReqid;
   });
   res.send(y);
 });
 
+// คำสั่งให้แสดงผลลงบน server ผ่าน port ที่กำหนด
 app.listen(port, () => {
   console.log("sever is running on port", port);
 });
--- src/inventory.js
+++ src/inventory.js
@@ -15,7 +15,10 @@ const listProduct = [
   },
 ];
 
-function nameproduct(name) {
+// ฟังก์ชันการเพิ่มสินค้า กรณีนี้ จะเพิ่มจากชื่อสินค้า แต่จะให้แสดงผลทั้ง {id, name} วิธีนี้จะใช้คู่กับ Method => Post
+// listProduct.length อ้างอิงถึงปริมาณในข้อมูลทั้งหมดใน listProduct
+// listProduct.push เพิ่มข้อมูลใน listProduct
+function addProduct(name) {
   const id = listProduct.length;
   listProduct.push({
     id: id,
@@ -25,51 +28,67 @@ function nameproduct(name) {
   return { id, name };
 }
 
-// สำหรับค้นหาแค่ตัวแปลในอาเรย์แบบเจาะจง
-// function updatedata(id,name) {
-//    const y = listProduct[] ลำดับ เริ่มนับจาก 0
+// ฟังก์ชัน แก้ไข(update)สินค้า วิธีนี้จะใช้คู่กับ Method => Put
+// ในการกำหนดฟังก์ชันเราจะต้องรู้ก่อนว่าจะแก้ไข(update)สินค้าตัวไหน
+// โดย user เป็นผู้ทำการกรอก id เพื่อร้องขอสินค้ามา และจะทำการแก้ไข(update) name ของสินค้านั้น
 
-// อัปเดตข้อมูลสินค้า โดยหาสินค้าที่มีidที่userกำหนดและอัปเดตnameข้อมูลของสินค้านั้น
+// ขั้นตอนเขียน code
+// 1.ใส่ function updatedata(id, name) {}
+// 2.findindex คือค้นหาลำดับที่เป็นชุดข้อมูล object{} ใน array[] ที่ชื่อว่า listProduct (จะเริ่มนับลำดับจาก 0 , 1 , 2 , 3... เป็นต้น) => ตัวแปล x = ค่าลำดับ
+// แต่ถ้าไม่เจอลำดับที่ user ร้องขอมา ระบบจะแสดงผล -1 กลับไป (แปลว่าชุดข้อมูลใน array[] ไม่มีลำดับที่ user ร้องขอมา)
+// 3.return Number(id) === product.id
+// คือการแปลงค่าที่ user มักจะเป็น string เพื่อให้แสดงผลได้ถูกต้อง จึงแปลงค่าที่ใส่เข้ามาเป็นตัวเลข Number(id)
+
+// ตัวอย่าง
+// ค่าที่ได้จาก URL หรือ query string มักจะเป็น string เช่น "5"
+// "5" === 5      // false (เพราะชนิดต่างกัน)
+// Number("5") === 5  // true
+
 function updatedata(id, name) {
-  // findindex คือ หาลำดับ  ค่าที่ได้จากตัวแปลxจะเท่ากับ = ค่าลำดับ แต่ถ้าไม่เจอจะส่งค่า -1 กลับมา
   const x = listProduct.findIndex((product) => {
-    // Number(id) คือการแปลงค่าที่ user ใส่เข้ามาและแปลงเป็นตัวเลข จากนั้นเปรียบเทียบค่าที่กรอกมานั้น เท่ากับเลขid
     return Number(id) === product.id;
   });
 
+  // splice ทำหน้าที่ลบค่าในอาเรย์และอัปเดทค่าในอาเรย์
+  // listProduct.splice (x,y,z) x = ลำดับข้อมูลที่จะอัปเดต , y = จำนวนสมาชิกที่ต้องการลบออกจากอาเรย์ , z = สมาชิกใหม่ที่ต้องการเพิ่มเข้าไปแทนที่
+  // ถ้าเจอ index ที่ถูกต้อง ให้ทำการลบค่าที่ลำดับนั้นออกไป จำนวน 1 ลำดับ {id, name}เก่า และแทนที่ด้วย NewProduct {id, name}ใหม่
   if (x !== -1) {
     const newProduct = { id, name };
-    // splice ทำหน้าที่ลบค่าในอาเรย์ และ อัปเดทค่าในอาเรย์
-    listProduct.splice(x, 1, newProduct); //x = ลำดับข้อมูลที่จะอัปเดต , 1 = จำนวนข้อมูลที่จะอัปเดต , newProduct = { id, name } แทนที่ลำดับข้อมูลที่กำหนดไว้ นั้นคือxของเรา
+
+    listProduct.splice(x, 1, newProduct);
     return newProduct;
-  } else {
+  }
+  // ถ้าไม่เจอ index ที่ถูกต้อง ให้แสดงค่า null
+  else {
     return null;
   }
 }
 
-// ฟังก์ชันสำหรับลบข้อมูล
+// ฟังก์ชันสำหรับลบข้อมูล วิธีนี้จะใช้คู่กับ Method => Delete
 // 1.หาตำแหน่งของสินค้า โดยที่ลูกค้าบอกผ่านไอดี id
-// 2.กำหนดเงื่อนไข ถ้าหาสินค้าไม่เจอ ให้บอกลูกค้ากลับไปว่าไม่รู้  แต่  ถ้ารู้ให้ดำเนินการขั้นตอนถัดไป
-// 3.ให้ดำเนินการนำสินค้าที่พบตำแหน่งนั้นไปทิ้ง นั้นคือลบ {} แปลว่าลบสินค้านั้นออกไป
+// 2.กำหนดเงื่อนไข ถ้าหาสินค้าไม่เจอ ให้บอกลูกค้ากลับไปว่าไม่รู้  แต่  ถ้าพบให้ดำเนินการขั้นตอนถัดไป
+// 3.ให้ดำเนินการนำสินค้าที่พบตำแหน่งนั้นไปทิ้ง นั้นคือลบข้อมูลในรูปแบบ {}
 
 function deleteProduct(id) {
   const x = listProduct.findIndex((product) => {
     return Number(id) === product.id;
   });
 
+  // ถ้าเจอ index ที่ถูกต้อง ให้ทำการลบค่าที่ลำดับน้ำออกไป จำนวน 1 ลำดับ {id, name}
   if (x !== -1) {
-    // splice ทำหน้าที่ลบค่าในอาเรย์ และ อัปเดทค่าในอาเรย์
-    //x = ลำดับข้อมูลที่จะอัปเดต , 1 = จำนวนข้อมูลที่จะอัปเดต , newProduct = { id, name } แทนที่ลำดับข้อมูลที่กำหนดไว้ นั้นคือxของเรา
     const deletedProduct = listProduct.splice(x, 1);
     return deletedProduct;
-  } else {
+  }
+
+  // ถ้าไม่เจอ index ที่ถูกต้อง ให้แสดงค่า null
+  else {
     return null;
   }
 }
 
 module.exports = {
   listProduct,
-  nameproduct,
+  addProduct,
   updatedata,
   deleteProduct,
 };
